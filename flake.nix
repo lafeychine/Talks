c302ce89a241fc0ff800847bf25cf3c8fa314211
Vincent Lafeychine <vincent.lafeychine@gmail.com>
2021-01-05 20:30:00 +0100

    Add nix flake - Update gitignore

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Lafeychine's Talks";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, flake-utils, nixpkgs }:
+
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+          fonts = pkgs.makeFontsConf { fontDirectories = with pkgs; [ lmodern fira fira-mono ]; };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [ inkscape gnuplot python3Packages.pygments texlive.combined.scheme-full ];
+
+	  shellHook = ''
+	    export FONTCONFIG_FILE=${fonts}
+	  '';
+        };
+      });
+}
